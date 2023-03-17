348454c1dfe61dd1162fc5fabe83890d18f6c0c5
mrprotocoll <lekanvgbg@gmail.com>
2023-03-17 17:45:08 +0100

    Displayed stock items on home page from the store

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,12 @@
 import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { getStocks } from 'redux/stock/stocksSlice';
 import styles from 'styles/Home.module.css';
 import Navbar from './Navbar';
 import StockItem from './StockItem';
 
 const Home = () => {
-  // const stocks = useSelector((store) => store.stocks);
+  const stocks = useSelector((store) => store.stocks);
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(getStocks());
@@ -16,9 +16,10 @@ const Home = () => {
     <div className="container">
       <Navbar />
       <div className={styles.items}>
-        <StockItem name="Apple" price="32,000" />
-        <StockItem name="Apple" price="32,000" />
-        <StockItem name="Apple" price="32,000" />
+        {
+          stocks.map((item) => <StockItem key={item.symbol} name={item.name} price={item.price} />)
+        }
+
       </div>
     </div>
   );
